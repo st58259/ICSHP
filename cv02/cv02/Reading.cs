d390b0ac6f6b319b5c27b1d420644e480d1132f7
st58259 <72407543+st58259@users.noreply.github.com>
2021-02-28 11:26:53 +0100

    Add files via upload

---
+++ cv02/cv02/Reading.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Fei.BaseLib
+{
+    public class Reading
+    {
+        //Takes string returns int 
+        public static int ReadInt(string ins)
+        {
+            Console.Write(ins + " : ");
+            string s = Console.ReadLine();
+
+            if (int.TryParse(s, out int res))
+            {
+                return res;
+            }
+
+            throw new ArgumentException();
+        }
+
+        //Takes string returns double 
+        public static double ReadDouble(string ins)
+        {
+            Console.Write(ins + " : ");
+            string s = Console.ReadLine();
+
+            if (double.TryParse(s, out double res))
+            {
+                return res;
+            }
+
+            throw new ArgumentException();
+        }
+
+        //Takes string returns char 
+        public static char ReadChar(string ins)
+        {
+            Console.Write(ins + " : ");
+            string s = Console.ReadLine();
+
+            if (char.TryParse(s, out char res))
+            {
+                return res;
+            }
+
+            throw new ArgumentException();
+        }
+
+        //Takes string returns string
+        public static string ReadString(string ins)
+        {
+            Console.Write(ins + " : ");
+            string s = Console.ReadLine();
+            return s;
+        }
+
+
+
+    }
+}
